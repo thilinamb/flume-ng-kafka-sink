38c92514b7ad9bd783cb6face5f375ffda406a86
Thilina Buddhika <thilinamb@gmail.com>
2014-08-14 14:55:24 -0600

    Adding license header.

--- impl/src/test/java/com.thilinamb.flume.sink/util/TestUtil.java
+++ impl/src/test/java/com.thilinamb.flume.sink/util/TestUtil.java
@@ -1,3 +1,21 @@
+/**
+ Licensed to the Apache Software Foundation (ASF) under one or more
+ contributor license agreements.  See the NOTICE file distributed with
+ this work for additional information regarding copyright ownership.
+ The ASF licenses this file to You under the Apache License, Version 2.0
+ (the "License"); you may not use this file except in compliance with
+ the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ limitations under the License.
+ */
+
 package com.thilinamb.flume.sink.util;
 
 import com.thilinamb.flume.sink.util.consumer.KafkaConsumer;
@@ -17,11 +35,9 @@ public class TestUtil {
     private static TestUtil instance = new TestUtil();
 
     private KafkaLocal kafkaServer;
-    private ZooKeeperLocal zookeeper;
     private KafkaConsumer kafkaConsumer;
 
     private TestUtil(){
-
     }
 
     public static TestUtil getInstance(){
@@ -38,7 +54,7 @@ public class TestUtil {
             zkProperties.load(Class.class.getResourceAsStream("/zookeeper.properties"));
 
             //start local zookeeper
-            zookeeper = new ZooKeeperLocal(zkProperties);
+            ZooKeeperLocal zookeeper = new ZooKeeperLocal(zkProperties);
             logger.info("ZooKeeper instance is started.");
             //start kafkaServer
             kafkaServer = new KafkaLocal(kafkaProperties);
@@ -50,10 +66,6 @@ public class TestUtil {
             logger.error("Error starting the Kafka Server.", e);
             return false;
         }
-    }
-
-    private void shutdownKafkaServer(){
-        kafkaServer.stop();
     }
 
     private KafkaConsumer getKafkaConsumer(){
